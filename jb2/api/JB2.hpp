5f1d251c7df43872dd8543cf9b4fc86c3e09c918
mstoodle <mstoodle@ca.ibm.com>
2022-12-20 15:49:37 -0500

    Create temporary memory allocator tracker JB2
    Signed-off-by: mstoodle <mstoodle@ca.ibm.com>

--- jb2/api/JB2.hpp
+++ jb2/api/JB2.hpp
@@ -19,31 +19,57 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-#ifndef TYPEDEFS_INCL
-#define TYPEDEFS_INCL
+#ifndef JB2_INCL
+#define JB2_INCL
 
-#include <vector>
-#include "IDs.hpp"
-#include "util/List.hpp"
+#include <cassert>
+#include <cstddef>
 
 namespace OMR {
 namespace JitBuilder {
 
-class Builder;
-typedef std::vector<Builder *> BuilderVector;
-typedef List<Builder *> BuilderWorklist;
+class Compiler;
 
-typedef uint64_t EyeCatcher;
+// eventually shift memory tracking to Compiler using an object rather than static
+class JB2 {
+    friend class Compiler;
 
-typedef uint8_t LiteralBytes;
-class Literal;
-typedef std::vector<Literal *> LiteralVector;
+public:
 
-class Symbol;
-typedef std::vector<Symbol *> SymbolVector;
+    template<typename T>
+    static T *allocate(size_t numItems) {
+        size_t bytes = numItems * sizeof(T);
+        if (bytes == 0)
+            return NULL;
+
+        T *p = (T *) malloc(bytes);
+        assert(p != NULL);
+        _totalAllocations++;
+        _totalAllocated += bytes;
+        return p;
+    }
+
+    template<typename T>
+    static void deallocate(T *ptr, size_t numItems) {
+        assert(ptr != NULL);
+        free(ptr);
+        _totalDeallocations++;
+        _totalDeallocated += numItems * sizeof(T);
+    }
+
+    static void report();
+
+protected:
+    JB2();
+
+    static size_t _totalAllocated;
+    static size_t _totalDeallocated;
+
+    static size_t _totalAllocations;
+    static size_t _totalDeallocations;
+};
 
 } // namespace JitBuilder
 } // namespace OMR
 
-#endif // defined(TYPEDEFS_INCL)
-
+#endif // defined(JB2_INCL)
