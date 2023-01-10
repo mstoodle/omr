c74d14cfaa4ee30a32dda9d3da8e4021cd0d0f89
mstoodle <mstoodle@ca.ibm.com>
2023-01-10 18:50:13 -0500

    Fix memory initialize/shutdown leaks in JitBuilder
    Valgrind points out multiple leaks that occur when just initializing
    and shutting down JitBuilder. These changes eliminate leaks in the
    code cache manager and code cache, the compilation controller, the
    CompilerEnv, and the CPU object on x86. The shutdown phase for
    JitBuilder gets a few more "shutdown" and "destroy" calls to properly
    free memory allocated during initialization.
    
    While these won't be the only leaks, it is a good step forward
    ensuring JitBuilder does not leak memory in the runtime process in
    which it runs. valgrind now reports zero leaked memory for successive
    initialize/shutdown sequences. These leaks probably impact other
    compilers as well, though the overall amount of leaked memory in
    my tests was only a few thousand bytes and most compilers only
    initialize/shutdown once and shutdown just ahead of process teardown.
    
    Fixes: #1190
    
    Signed-off-by: mstoodle <mstoodle@ca.ibm.com>

--- compiler/x/env/OMRCPU.cpp
+++ compiler/x/env/OMRCPU.cpp
@@ -80,8 +80,6 @@ OMR::X86::CPU::initializeTargetProcessorInfo(bool force)
    OMR::X86::CodeGenerator::initializeX86TargetProcessorInfo(force);
    }
 
-TR_X86CPUIDBuffer *OMR::X86::CPU::cpuIDBuf = NULL;
-
 void
 OMR::X86::CPU::destroyTargetProcessorInfo()
    {
@@ -89,6 +87,8 @@ OMR::X86::CPU::destroyTargetProcessorInfo()
       free(cpuIDBuf);
    cpuIDBuf = NULL;
    }
+
+TR_X86CPUIDBuffer *OMR::X86::CPU::cpuIDBuf = NULL;
 
 TR_X86CPUIDBuffer *
 OMR::X86::CPU::queryX86TargetCPUID()
